f37221ab3eb056bd3947d6a3f2bcf1264aa9382b
Rishi Raman Sinha <152724109+rishiraman27@users.noreply.github.com>
2026-02-16 22:37:54 +0530

    SIGNUP controller refactoring

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDb = async()=>{
     try{
+        const {MONGO_URL}= process.env;
+        if(!MONGO_URL){
+            throw new Error("MONGO_URL is not defined in environment variables");
+        }
         const conn= await mongoose.connect(process.env.MONGO_URL)
         console.log("Connected to MongoDB", conn.connection.host);
     }catch(error){
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,6 +1,10 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken= (userId,res)=>{
+    const {JWT_SECRET, NODE_ENV}= process.env;
+    if(!JWT_SECRET){
+        throw new Error("JWT_SECRET is not defined in environment variables");
+    }
     const token= jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn:"7d",});
     
     res.cookie("jwt", token ,{
